4ad3aa835ab91c56554e9df5e69a2cc0b56c864f
Coop Williams <ken.c.williams.iii@gmail.com>
2020-03-13 11:40:41 -0400

    satisfy weight limit

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,5 +1,6 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
+                        hash,
                         hash_table_insert,
                         hash_table_remove,
                         hash_table_retrieve,
@@ -12,7 +13,17 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    for i, x in enumerate(weights):
+        hash_table_insert(ht, x, i)
 
+    # pick weight below limit
+    for i_1, weight_1 in enumerate(weights):
+        if weight_1 > limit:
+            pass
+        # get the index of its complement from the hashtable
+        i_2 = hash_table_retrieve(ht, limit-weight_1)
+        if i_2 is not None:
+            return i_2, i_1
     return None
 
 
